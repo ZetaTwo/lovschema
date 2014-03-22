d0798075e9bf53016def3487ec0eea236e74aeae
Calle Svensson <calle.svensson@zeta-two.com>
2014-03-22 19:14:22 +0100

    Shutdown close db.

--- webapp.js
+++ webapp.js
@@ -6,6 +6,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 
