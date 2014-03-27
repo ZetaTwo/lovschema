7101abaa950b20c04533a6e0f881c7fa4ac907cf
Calle Svensson <calle.svensson@zeta-two.com>
2014-03-27 01:07:55 +0100

    Prepared for publication

--- config.sample.js
+++ config.sample.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose');
 
 //Database
 db_config = {
-  HOST: "localhost",
-  DATABASE: "lovschema"
+  HOST: "",
+  DATABASE: ""
 };
 mongoose.connect('mongodb://' + db_config.HOST + '/' + db_config.DATABASE);
 exports.mongoose = mongoose;
 
 //Google
 exports.google = {
-  API_KEY: 'AIzaSyA9BMKIQb6Kk5uTySWXp9kWtTn4dSRojzQ',
+  API_KEY: '',
   CLIENT_ID: "",
   CLIENT_SECRET: "",
   REDIRECT_URL: ""
